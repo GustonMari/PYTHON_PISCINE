3b30d0e262eb0516b465799ef6d0bc5c374664e7
gustave mary <gustave1mary@gmail.com>
2023-07-12 06:33:39 +0200

    python04 ex00 done

--- PYTHON_04/ex00/statistics.py
+++ PYTHON_04/ex00/statistics.py
@@ -3,23 +3,31 @@
 def ft_mean(*args: any) -> float:
     """function mean"""
     if len(args) == 0:
+        print("ERROR")
         return None
     res = sum(args) / len(args)
     return res
+
 
 # ! technique tres cool pour la mediane
 # ! https://www.geeksforgeeks.org/find-median-of-list-in-python/
 def ft_median(*args: any):
     """function median"""
+    if len(args) == 0:
+        print("ERROR")
+        return None
     args = sorted(args)
-    # args.sort()
     mid = len(args) // 2
     res = (args[mid] + args[~mid]) / 2
     print("median: ", res)
 
+
 # ! this operator // is floor division, it returns the quotient
 def ft_quartile(*args: any):
     """function quartile"""
+    if len(args) == 0:
+        print("ERROR")
+        return None
     args = sorted(args)
     args_len = len(args)
     q1 = args[(args_len + 1) // 4]
@@ -29,30 +37,41 @@ def ft_quartile(*args: any):
     res.append(float(q3))
     print("quartile: ", res)
 
+
 def ft_var(*args: any) -> float:
+    """function variance"""
+    if len(args) == 0:
+        print("ERROR")
+        return None
     mean = ft_mean(*args)
     tmp_list = args
     tmp_list = [x - mean for x in tmp_list]
     tmp_list = [x ** 2 for x in tmp_list]
-    res = sum([x for x in tmp_list]) 
+    res = sum([x for x in tmp_list])
     # ? for this exercise we work on a population so just N
     res = res / (len(tmp_list))
     return res
 
+
 # ! https://www.scribbr.com/statistics/standard-deviation/
 def ft_std(*args: any):
+    """standard deviation"""
+    if len(args) == 0:
+        print("ERROR")
+        return None
     res = ft_var(*args) ** 0.5
     print("std: ", res)
 
 
 def ft_statistics(*args: any, **kwargs: any) -> None:
     """function statistics"""
-    if len(args) == 0:
-        return None
     for x in kwargs.values():
         match x:
             case "mean":
-                print("mean :", ft_mean(*args))
+                if len(args) == 0:
+                    print("ERROR")
+                else:
+                    print("mean :", ft_mean(*args))
             case "median":
                 ft_median(*args)
             case "quartile":
@@ -60,7 +79,7 @@ def ft_statistics(*args: any, **kwargs: any) -> None:
             case "std":
                 ft_std(*args)
             case "var":
-                print("var: ", ft_var(*args))
-            case default:
-                # print("error")
-                return None
+                if len(args) == 0:
+                    print("ERROR")
+                else:
+                    print("var: ", ft_var(*args))
